a72463d354c2befc3ec4c36d53530112a00bd5d3
Galina Ivanova <52123474+GalyaIT@users.noreply.github.com>
2023-09-12 22:16:22 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import { v4 as uuidv4 } from "uuid";
 import './App.css';
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
+import TodosWrapper from './components/TodosWrapper';
 
 function App() {
   const [todos, setTodos] = useState(() => {
@@ -15,39 +13,9 @@ function App() {
     localStorage.setItem("ITEMS", JSON.stringify(todos))
   }, [todos])
 
-  function addTodo(title) {
-    setTodos(currentTodos => {
-      return [
-        ...currentTodos,
-        { id:uuidv4(), title, completed: false },
-      ]
-    })
-  }
-  function toggleTodo(id, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, completed }
-        }      
-        return todo
-      })
-    })
-  }
-
-  function deleteTodo(id) {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
-    })
-  }
-  console.log(todos);
   return (
-    <div className="todo__app">
-      <div className='todo__app-wrapper'>
-         <h1 style={{color:"white", textAlign:"center"}}>What's the plan for today?</h1>
-      <TodoForm onSubmit={addTodo}/> 
-      <h3 style={{color:"white",textAlign:"center"}}>Todo List</h3>     
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
-      </div>     
+    <div className="todo__app">      
+      <TodosWrapper todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
---
+++ src/components/TodosWrapper.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { v4 as uuidv4 } from "uuid";
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+
+const TodosWrapper = ({todos, setTodos}) => {
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id:uuidv4(), title, completed: false },
+      ]
+    })
+  }
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }      
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+  return (
+       <div className='todo__app-wrapper'>
+         <h1 style={{color:"white", textAlign:"center"}}>What's the plan for today?</h1>
+      <TodoForm onSubmit={addTodo}/> 
+      <h3 style={{color:"white",textAlign:"center"}}>Todo List</h3>     
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+      </div>     
+  )
+}
+
+export default TodosWrapper
